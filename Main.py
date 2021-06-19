66ade3fabd3217aedbc7ab37e945f566653daafb
Evgeniy Zavarin <zavarin.ed@phystech.edu>
2021-06-19 22:34:03 +0300

    Добавление библиотеки

--- Main.py
+++ Main.py
@@ -8,7 +8,7 @@ def send_welcome(message):
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text.lower() == 'привет':
-        bot.send_message(message.from_user.id, 'Привет!')
+        bot.send_message(message.from_user.id, 'Привет')
     else:
         bot.send_message(message.from_user.id, 'Не понимаю, что это значит.')
 
